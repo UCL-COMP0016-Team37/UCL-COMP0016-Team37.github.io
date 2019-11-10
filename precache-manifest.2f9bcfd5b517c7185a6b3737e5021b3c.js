6f436252cbf6bddab8925112853a92d849186b28
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-11-10 17:44:56 +0000

    Initial commit

---
+++ precache-manifest.2f9bcfd5b517c7185a6b3737e5021b3c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c8f06329d41bea4e15e8c291541fd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11f5ce5137204000d3ed",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "9ee96e3a07dec6235d3f1a86787a92b7",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "3b973a05c0e1544f7ee8fa23c2a9315a",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "535bcf7dd3feb41ec20507c3c5b81efb",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "1dfb73e22b9119f547fb880568f56ea2",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "11f5ce5137204000d3ed",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "7ec80ffbc1d076f1d6beb230a75d859f",
+    "url": "js/app.js.LICENSE"
+  },
+  {
+    "revision": "ef2315dcab6011928a1f19dda85d355e",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "4e35a6dad5f1ab5b385f86dfe97579b8",
+    "url": "static/icons/128x128.png"
+  },
+  {
+    "revision": "47efa07843a29aff095e50015e084e85",
+    "url": "static/icons/144x144.png"
+  },
+  {
+    "revision": "ab189ff1c3604cbecd2ccc180b6f7c25",
+    "url": "static/icons/152x152.png"
+  },
+  {
+    "revision": "9ad7d46019f56396237834ced5038973",
+    "url": "static/icons/192x192.png"
+  },
+  {
+    "revision": "28969ffd71e59d0bb2ca642ac8bb0134",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "aa0c2f038e42624eb7ee396b272ee852",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "4e94b1d1edaea36f052ec015c03ff26b",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4e35a6dad5f1ab5b385f86dfe97579b8",
+    "url": "static/icons/favicon.png"
+  },
+  {
+    "revision": "87a1ad87e9b31621aa09f3d2a0d04dfd",
+    "url": "static/images/UserInfo.jpeg"
+  },
+  {
+    "revision": "49fa548bf2ad23532c689854536b89c0",
+    "url": "static/images/avatarCn.jpeg"
+  },
+  {
+    "revision": "fbf342363c9f1950b2afbf91b6f7bccb",
+    "url": "static/images/avatarUk.jpeg"
+  },
+  {
+    "revision": "a81246911dfee3c99261c454e50b404e",
+    "url": "static/images/avatarUk1.jpeg"
+  },
+  {
+    "revision": "95ad4572277cc34cc47728bc72754fe1",
+    "url": "static/images/frenchFood.jpg"
+  },
+  {
+    "revision": "835cd8c41d1e87c036b325f5f7c1a4dc",
+    "url": "static/images/frenchRestaurant.jpg"
+  },
+  {
+    "revision": "f74b79a6ec8f4a65950b5890d5344631",
+    "url": "static/images/frenchSite.jpeg"
+  },
+  {
+    "revision": "7d884118dad367225fb5eafadd128ab5",
+    "url": "static/json/frenchFood.json"
+  },
+  {
+    "revision": "88c19a0c0863b22a55b76dce61261700",
+    "url": "static/json/frenchSite.json"
+  }
+]);
